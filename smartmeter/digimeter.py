65c7d810c065c7ce393ddab295721469add0e369
Wim De Hul <wim.de.hul@gmail.com>
2023-02-13 22:43:03 +0100

    fixes in teimestamp format

--- smartmeter/digimeter.py
+++ smartmeter/digimeter.py
@@ -74,6 +74,7 @@ def convert_timestamp(timestamp: str, format: Optional[str] = None) -> str:
         raise ValueError("Timestamp cannot be empty.")
 
     tz_hour_map = {"S": "02:00", "W": "01:00"}
+    tz_zone_map = {"S": "CEST", "W": "CET"}
     year = "20" + timestamp[0:2]
     month = timestamp[2:4]
     day = timestamp[4:6]
@@ -81,11 +82,12 @@ def convert_timestamp(timestamp: str, format: Optional[str] = None) -> str:
     minute = timestamp[8:10]
     second = timestamp[10:12]
     tz_hours = tz_hour_map[timestamp[-1]]
+    tz_zone = tz_zone_map[timestamp[-1]]
 
     if format == "iso8601":
         result = f"{year}-{month}-{day}T{hour}:{minute}:{second}+{tz_hours}"
     else:
-        result = f"{year}-{month}-{day} {hour}:{minute}:{second}"
+        result = f"{year}-{month}-{day} {hour}:{minute}:{second} {tz_zone}"
 
     LOG.debug("Converted timestamp from %s to %s", timestamp, result)
 
