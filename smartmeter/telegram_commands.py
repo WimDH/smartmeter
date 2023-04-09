86f930963c4f8b3950d2d5f59305852af1800fd2
Wim De Hul <wim.de.hul@gmail.com>
2023-04-09 20:53:37 +0200

    small improvements

--- smartmeter/telegram_commands.py
+++ smartmeter/telegram_commands.py
@@ -11,22 +11,22 @@ def generate_status_message() -> str:
     Generate the HTLM for the Telegram answer.
     """
     status = Status()
-    output_lines = ["<b>System</b>", f"<b>up since</b>: {status.system['up_since']}"]
+    output_lines = ["<b>System</b>", f"up since: {status.system['up_since']}"]
 
     output_lines.append("<b>Load status</b>")
 
     for load_name, load_data in status.loads.items():
         load_stat = "ON" if load_data["state"] else "OFF"
-        time_human = human_time_duration(load_data['current_state_time']) if load_data['current_state_time'] else "None"
+        time_human = human_time_duration(load_data['current_state_time']) if load_data['current_state_time'] else "(no data)"
         output_lines.append(
             f"load {load_name}: {load_stat} for {time_human}"
         )
 
     output_lines.append("<b>Meter data</b>")
     if status.meter["actual_total_consumption"] > 0:
-        output_lines.append(f"Actual consumption: {status.meter['actual_total_consumption']} Watt.")
+        output_lines.append(f"Actual consumption: {status.meter['actual_total_consumption']} kW.")
     else:
-        output_lines.append(f"Actual injection: {status.meter['actual_total_injection']} Watt.")
+        output_lines.append(f"Actual injection: {status.meter['actual_total_injection']} kW.")
 
     output_lines.append(
         f"Actual current L1/L2/L3: {status.meter['l1_current']}A/{status.meter['l2_current']}A/{status.meter['l3_current']}A ."
