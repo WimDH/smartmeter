b750612abb5f796c77ec2b594fe99005f92bde2f
Wim De Hul <wim.de.hul@gmail.com>
2023-09-17 20:51:54 +0200

    wip

--- smartmeter/telegram_commands.py
+++ smartmeter/telegram_commands.py
@@ -34,16 +34,17 @@ def generate_status_message() -> str:
             f"Actual current L1/L2/L3: {status.meter['l1_current']}A/{status.meter['l2_current']}A/{status.meter['l3_current']}A."
         )
 
+        # Sensor data:
+        output_lines.append(
+            "<b>Sensor data</b>",
+            f"Current sensors: Car: {status.sensors['current_car']}A, VVP: {status.sensors['current_vvp']}A."
+        )
+
+        return "\n".join(output_lines)
+
     except Exception:
         LOG.exception("Error building status message!")
-
-    # Sensor data:
-    output_lines.append(
-        "<b>Sensor data</b>",
-        f"Current sensors: Car: {status.sensors['current_car']}A, VVP: {status.sensors['current_vvp']}A."
-    )
-
-    return "\n".join(output_lines)
+        return 'Internal Error!'
 
 
 async def status(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
